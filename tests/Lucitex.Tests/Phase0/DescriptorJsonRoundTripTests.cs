a5889ea231c3e81d2504ac8610cd2f2cfd5fb725
Sieluna <seele.peng@gmail.com>
2023-07-10 12:07:34 +0300

    introduce phase 0 freeze-gate tests

---
+++ tests/Lucitex.Tests/Phase0/DescriptorJsonRoundTripTests.cs
@@ -0,0 +1,44 @@
+using Lucitex.Core.Serialization;
+using Lucitex.Tests.Fixtures;
+
+namespace Lucitex.Tests.Phase0;
+
+public class DescriptorJsonRoundTripTests
+{
+    [Theory]
+    [MemberData(nameof(FixtureCatalog.Keys), MemberType = typeof(FixtureCatalog))]
+    public void Serialize_Deserialize_Serialize_ProducesIdenticalJson(string fixtureKey)
+    {
+        var asset = FixtureCatalog.All[fixtureKey]();
+
+        var firstJson = DescriptorJsonSerializer.Serialize(asset);
+        var roundTripped = DescriptorJsonSerializer.Deserialize(firstJson);
+        var secondJson = DescriptorJsonSerializer.Serialize(roundTripped);
+
+        Assert.Equal(firstJson, secondJson);
+    }
+
+    [Theory]
+    [MemberData(nameof(FixtureCatalog.Keys), MemberType = typeof(FixtureCatalog))]
+    public void Serialize_EmbedsCurrentSchemaVersion(string fixtureKey)
+    {
+        var asset = FixtureCatalog.All[fixtureKey]();
+
+        var json = DescriptorJsonSerializer.Serialize(asset);
+
+        Assert.Contains(DescriptorDocument.CurrentSchemaVersion, json);
+    }
+
+    [Fact]
+    public void Deserialize_RejectsUnknownSchemaVersion()
+    {
+        const string json = """
+        {
+          "schemaVersion": "graphics.imageio.descriptor/999",
+          "asset": { "parts": [] }
+        }
+        """;
+
+        Assert.Throws<System.Text.Json.JsonException>(() => DescriptorJsonSerializer.Deserialize(json));
+    }
+}
---
+++ tests/Lucitex.Tests/Phase0/RepresentationCoverageTests.cs
@@ -0,0 +1,67 @@
+using Lucitex.Core.Representation;
+using Lucitex.Tests.Fixtures;
+
+namespace Lucitex.Tests.Phase0;
+
+public class RepresentationCoverageTests
+{
+    [Fact]
+    public void Fixtures_CoverPlainSampleRepresentation()
+    {
+        Assert.Contains(FixtureCatalog.All.Values, build => build().Parts[0].Representation is PlainSampleRepresentation);
+    }
+
+    [Fact]
+    public void Fixtures_CoverPackedEncodedRepresentation()
+    {
+        Assert.Contains(
+            FixtureCatalog.All.Values,
+            build => build().Parts[0].Representation is EncodedElementRepresentation { Class: EncodedElementClass.Packed });
+    }
+
+    [Fact]
+    public void Fixtures_CoverBlockCompressedRepresentation()
+    {
+        Assert.Contains(
+            FixtureCatalog.All.Values,
+            build => build().Parts[0].Representation is EncodedElementRepresentation { Class: EncodedElementClass.BlockCompressed });
+    }
+
+    [Fact]
+    public void Fixtures_CoverSharedExponentRepresentation()
+    {
+        Assert.Contains(
+            FixtureCatalog.All.Values,
+            build => build().Parts[0].Representation is EncodedElementRepresentation { Class: EncodedElementClass.SharedExponent });
+    }
+
+    [Fact]
+    public void Fixtures_CoverIndexedRepresentation()
+    {
+        Assert.Contains(FixtureCatalog.All.Values, build => build().Parts[0].Representation is IndexedRepresentation);
+    }
+
+    [Fact]
+    public void Fixtures_CoverDeepRepresentation()
+    {
+        Assert.Contains(FixtureCatalog.All.Values, build => build().Parts[0].Representation is DeepRepresentation);
+    }
+
+    [Fact]
+    public void Fixtures_CoverSubsampledChannels()
+    {
+        var asset = ExrFixtures.SubsampledChannels();
+        var channels = asset.Parts[0].Channels.Channels;
+
+        Assert.Contains(channels, c => c.Sampling.Step is { X: 2, Y: 2 });
+        Assert.Contains(channels, c => c.Sampling.Step is { X: 1, Y: 1 });
+    }
+
+    [Fact]
+    public void Fixtures_CoverMultipleParts()
+    {
+        var asset = ExrFixtures.Multipart();
+
+        Assert.True(asset.Parts.Count > 1);
+    }
+}

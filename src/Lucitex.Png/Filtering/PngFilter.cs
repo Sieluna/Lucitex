4dbb1560584efec90f38345f6c440f3760be5df5
Sieluna <seele.peng@gmail.com>
2023-09-15 09:35:00 +0300

    vectorize png scanline filters and stream-decode inflated pixel data

--- src/Lucitex.Png/Filtering/PngFilter.cs
+++ src/Lucitex.Png/Filtering/PngFilter.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Lucitex.Core.Execution;
 using Lucitex.Png.Format;
 
@@ -17,10 +18,7 @@ internal static class PngFilter
 
                 break;
             case PngFilterType.Up:
-                for (var i = 0; i < current.Length; i++) {
-                    var up = previous.IsEmpty ? 0 : previous[i];
-                    current[i] = unchecked((byte)(current[i] + up));
-                }
+                ReconstructUp(current, previous);
 
                 break;
             case PngFilterType.Average:
@@ -52,25 +50,15 @@ internal static class PngFilter
                 raw.CopyTo(output);
                 break;
             case PngFilterType.Sub:
-                for (var i = 0; i < raw.Length; i++) {
-                    var left = i >= bpp ? raw[i - bpp] : 0;
-                    output[i] = unchecked((byte)(raw[i] - left));
-                }
+                ApplySub(output, raw, bpp);
 
                 break;
             case PngFilterType.Up:
-                for (var i = 0; i < raw.Length; i++) {
-                    var up = previousRaw.IsEmpty ? 0 : previousRaw[i];
-                    output[i] = unchecked((byte)(raw[i] - up));
-                }
+                ApplyUp(output, raw, previousRaw);
 
                 break;
             case PngFilterType.Average:
-                for (var i = 0; i < raw.Length; i++) {
-                    var left = i >= bpp ? raw[i - bpp] : 0;
-                    var up = previousRaw.IsEmpty ? 0 : previousRaw[i];
-                    output[i] = unchecked((byte)(raw[i] - ((left + up) / 2)));
-                }
+                ApplyAverage(output, raw, previousRaw, bpp);
 
                 break;
             case PngFilterType.Paeth:
@@ -84,6 +72,92 @@ internal static class PngFilter
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(filterType));
+        }
+    }
+
+    private static void ReconstructUp(Span<byte> current, ReadOnlySpan<byte> previous)
+    {
+        if (previous.IsEmpty) {
+            return;
+        }
+
+        var i = 0;
+        var lanes = Vector<byte>.Count;
+        if (Vector.IsHardwareAccelerated) {
+            for (; i <= current.Length - lanes; i += lanes) {
+                var value = new Vector<byte>(current.Slice(i, lanes));
+                var up = new Vector<byte>(previous.Slice(i, lanes));
+                (value + up).CopyTo(current.Slice(i, lanes));
+            }
+        }
+
+        for (; i < current.Length; i++) {
+            current[i] = unchecked((byte)(current[i] + previous[i]));
+        }
+    }
+
+    private static void ApplySub(Span<byte> output, ReadOnlySpan<byte> raw, int bpp)
+    {
+        raw[..Math.Min(bpp, raw.Length)].CopyTo(output);
+        var i = bpp;
+        var lanes = Vector<byte>.Count;
+        if (Vector.IsHardwareAccelerated) {
+            for (; i <= raw.Length - lanes; i += lanes) {
+                var value = new Vector<byte>(raw.Slice(i, lanes));
+                var left = new Vector<byte>(raw.Slice(i - bpp, lanes));
+                (value - left).CopyTo(output.Slice(i, lanes));
+            }
+        }
+
+        for (; i < raw.Length; i++) {
+            output[i] = unchecked((byte)(raw[i] - raw[i - bpp]));
+        }
+    }
+
+    private static void ApplyUp(Span<byte> output, ReadOnlySpan<byte> raw, ReadOnlySpan<byte> previous)
+    {
+        if (previous.IsEmpty) {
+            raw.CopyTo(output);
+            return;
+        }
+
+        var i = 0;
+        var lanes = Vector<byte>.Count;
+        if (Vector.IsHardwareAccelerated) {
+            for (; i <= raw.Length - lanes; i += lanes) {
+                var value = new Vector<byte>(raw.Slice(i, lanes));
+                var up = new Vector<byte>(previous.Slice(i, lanes));
+                (value - up).CopyTo(output.Slice(i, lanes));
+            }
+        }
+
+        for (; i < raw.Length; i++) {
+            output[i] = unchecked((byte)(raw[i] - previous[i]));
+        }
+    }
+
+    private static void ApplyAverage(Span<byte> output, ReadOnlySpan<byte> raw, ReadOnlySpan<byte> previous, int bpp)
+    {
+        var i = 0;
+        var lanes = Vector<byte>.Count;
+        if (Vector.IsHardwareAccelerated && !previous.IsEmpty) {
+            for (; i < Math.Min(bpp, raw.Length); i++) {
+                output[i] = unchecked((byte)(raw[i] - (previous[i] / 2)));
+            }
+
+            for (; i <= raw.Length - lanes; i += lanes) {
+                var value = new Vector<byte>(raw.Slice(i, lanes));
+                var left = new Vector<byte>(raw.Slice(i - bpp, lanes));
+                var up = new Vector<byte>(previous.Slice(i, lanes));
+                var average = (left & up) + ((left ^ up) >> 1);
+                (value - average).CopyTo(output.Slice(i, lanes));
+            }
+        }
+
+        for (; i < raw.Length; i++) {
+            var left = i >= bpp ? raw[i - bpp] : 0;
+            var up = previous.IsEmpty ? 0 : previous[i];
+            output[i] = unchecked((byte)(raw[i] - ((left + up) / 2)));
         }
     }
 

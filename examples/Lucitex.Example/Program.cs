d638d8a471e3da7d72fee8a71158292300e93991
Sieluna <seele.peng@gmail.com>
2023-09-27 22:15:00 +0300

    add an examples/lucitex.example console app demonstrating format-to-format conversion

---
+++ examples/Lucitex.Example/Program.cs
@@ -0,0 +1,67 @@
+using Lucitex.Conversion;
+using Lucitex.Core.Execution.Codecs;
+using Lucitex.Dds;
+using Lucitex.Exr;
+using Lucitex.Hdr;
+using Lucitex.Ktx2;
+using Lucitex.Png;
+
+if (args.Length != 2) {
+    Console.Error.WriteLine("usage: Lucitex.Example <source-image> <target-image>");
+    return 1;
+}
+
+var sourcePath = args[0];
+var targetPath = args[1];
+
+IImageCodec[] codecs = [new PngCodec(), new DdsCodec(), new ExrCodec(), new HdrCodec(), new Ktx2Codec()];
+
+try {
+    var sourceCodec = ResolveCodec(sourcePath);
+    var targetCodec = ResolveCodec(targetPath);
+
+    using var sourceStream = File.OpenRead(sourcePath);
+    var reader = sourceCodec.OpenReader(sourceStream);
+    var sourceDescriptor = reader.Describe();
+
+    var planResult = ConversionPlanner.Plan(sourceDescriptor, targetCodec.Capabilities, ConversionPolicy.Preview);
+    if (!planResult.Success) {
+        Console.Error.WriteLine($"cannot convert '{sourceCodec.FormatId}' to '{targetCodec.FormatId}':");
+        foreach (var diagnostic in planResult.Diagnostics) {
+            Console.Error.WriteLine($"  [{diagnostic.Category}] {diagnostic.Message}");
+        }
+
+        return 1;
+    }
+
+    var plan = planResult.Plan!;
+
+    using (var targetStream = File.Create(targetPath)) {
+        var writer = targetCodec.CreateWriter(targetStream, plan.TargetDescriptor);
+        ConversionExecutor.Execute(plan, reader, sourceCodec.Capabilities.SampleByteOrder, writer, targetCodec.Capabilities.SampleByteOrder);
+    }
+
+    var extent = sourceDescriptor.Parts[0].Topology.BaseExtent;
+    Console.WriteLine($"{sourceCodec.FormatId} -> {targetCodec.FormatId}: {extent.Width}x{extent.Height} ({plan.Policy}, plan {plan.PlanFingerprint[..12]})");
+
+    foreach (var diagnostic in plan.Diagnostics) {
+        Console.WriteLine($"  [{diagnostic.Category}] {diagnostic.Message}");
+    }
+
+    return 0;
+}
+catch (Exception ex) when (ex is NotSupportedException or IOException) {
+    Console.Error.WriteLine(ex.Message);
+    return 1;
+}
+
+IImageCodec ResolveCodec(string path)
+{
+    var extension = Path.GetExtension(path);
+    var codec = codecs.FirstOrDefault(c => c.Extensions.Contains(extension, StringComparer.OrdinalIgnoreCase));
+    if (codec is null) {
+        throw new NotSupportedException($"no codec registered for extension '{extension}'");
+    }
+
+    return codec;
+}
